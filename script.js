ebf118517429f0043a31f64cbd260f9c79ca83de
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-24 14:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
+// script.js
 
-    //Write your code here
-	   function getFormvalue() {
-            // Access the form element by its ID
-            var form = document.getElementById("form1");
+function getFormvalue(event) {
+    event.preventDefault(); // Prevent the default form submission
 
-            // Access the first name and last name input elements by their IDs
-            var firstNameInput = document.getElementById("firstName");
-            var lastNameInput = document.getElementById("lastName");
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
 
-            // Get the values of the first name and last name inputs
-            var firstName = firstNameInput.value;
-            var lastName = lastNameInput.value;
-
-            // Check if the inputs are not empty
-            if (FirstName !== "" && LastName !== "") {
-                // Display the first and last name using an alert
-                alert("First Name: " + FirstName + "\nLast Name: " + LastName);
-            } else {
-                alert("Please fill in both first name and last name fields.");
-            }
-
-            // Prevent the default form submission behavior
-            return false;
-        }
-
-        // Add an event listener to the form to call getFormvalue() when submitted
-        var form = document.getElementById("form1");
-        form.addEventListener("submit", getFormvalue);
-	
-
+    if (firstName && lastName) {
+        alert("First Name: " + firstName + "\nLast Name: " + lastName);
+    } else {
+        alert("Please fill in both first name and last name fields.");
+    }
 }
+
+var loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", getFormvalue);
